b04969da7babc17b69c35a5058b328e58d7f819c
Roman <dmot.boss@mail.ru>
2024-02-27 20:06:43 +0300

    add HomeWork7: Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿void Main()
+{
+    int M = ReadInt("Введите число M: ");
+    int N = ReadInt("Введите число N: ");
+    PrintNumbers(M, N);
+}
+
+int ReadInt(string str)
+{
+    Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNumbers(int m, int n)
+{
+    if(m > n) return;
+    
+    Console.Write(m + "\t");
+    PrintNumbers(m + 1, n);
+}
+
+Main();
