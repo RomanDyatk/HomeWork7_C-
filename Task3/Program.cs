ffd024dc6c80823b9a8a86a5552ecbd1c33508d6
Roman <dmot.boss@mail.ru>
2024-02-27 21:34:17 +0300

    add HomeWork7: Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿void Main()
+{
+    int length = 5;
+    int index = length - 1;
+    int[] array = new int[length];
+    FillingArray(array);
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "\t");
+    }
+
+    Console.WriteLine();
+
+    PrintArray(array, index);
+
+}
+
+void FillingArray(int[] array)
+{
+    Random rand = new Random();
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(0, 50);
+    }
+
+}
+
+void PrintArray(int[] array, int index)
+{
+    if(index < 0) return;
+    Console.Write(array[index] + "\t");
+    PrintArray(array, index - 1); 
+}
+
+Main();
